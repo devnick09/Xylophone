6fa6756d680139850098be61856359f7a939942c
Nikhil Sharma <nickbryne98@gmail.com>
2022-01-04 16:31:01 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+
+  void playSound(int sound){
+    final player = AudioCache();
+    player.play('note$sound.wav');
+  }
+
+  Expanded buildKey( Color color, int sound){
+   return Expanded(
+      child: FlatButton(
+        onPressed:(){
+          playSound(sound);
+        },
+        color: color,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
